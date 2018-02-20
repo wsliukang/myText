f2535435e1874bf64fbaabbb6328854572010fef
刘康 <1171788142@qq.com>
2018-02-20 20:14:13 +0800

    mysql数据库文件

---
+++ xiteng.sql
@@ -0,0 +1,84 @@
+﻿# Host: 127.0.0.1  (Version 5.5.15)
+# Date: 2018-02-20 20:11:13
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "tb_collection"
+#
+
+CREATE TABLE `tb_collection` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `news_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `brief` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "tb_comment"
+#
+
+CREATE TABLE `tb_comment` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `news_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT '匿名用户',
+  `content` varchar(255) DEFAULT NULL,
+  `favour` int(11) DEFAULT NULL,
+  `disfavour` int(11) DEFAULT NULL,
+  `comment_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "tb_headshot"
+#
+
+CREATE TABLE `tb_headshot` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "tb_news"
+#
+
+CREATE TABLE `tb_news` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `brief` varchar(255) DEFAULT NULL,
+  `author` varchar(255) DEFAULT '佚名',
+  `content` varchar(20000) DEFAULT NULL,
+  `publish_date` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "tb_user"
+#
+
+CREATE TABLE `tb_user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "tb_userinfo"
+#
+
+CREATE TABLE `tb_userinfo` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `sex` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `telephone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
